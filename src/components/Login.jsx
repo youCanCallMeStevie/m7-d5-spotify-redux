c8f8ca7a765c8598060d4db22665db61fe5e41cd
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-02-05 11:54:22 +0000

    controlled form added to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,73 +1,112 @@
 import Logo from "../assets/logo.png";
 import React, { Component } from "react";
 import { withRouter, Link } from "react-router-dom";
-import { InputGroup, FormControl, Button, Image} from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faHome, faBook, faHeart, faPlusCircle, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
+import { Button, Form } from "react-bootstrap";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faSearch, faHome, faBook, faHeart, faPlusCircle, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./CSS/Login.css";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => state;
+
+// const mapDispatchToProps = (dispatch) => ({
+//   setUserDetails: (username, password) =>
+//     dispatch({ type: SET_USER_DETAILS, payload: {username, password} }),
+// });
 
 export class Login extends Component {
-    render() {
-        return (
-            <body className="login-body justify-content-center">
-            <section>
-            <div className="login-logo">
-              <a href="index.html"><img src={Logo} /></a>
+  render() {
+    return (
+      <body className="login-body justify-content-center">
+        <section>
+          <div className="login-logo">
+            <a href="index.html">
+              <img src={Logo} />
+            </a>
+          </div>
+          <div className="login-container">
+            <span> To continue, log in to Spotify.</span>
+            <div className="login-button login-facebook">
+              <i className="fa fa-facebook"></i>
+              <span>CONTINUE WITH FACEBOOK</span>
             </div>
-            <div className="login-container">
-              <span> To continue, log in to Spotify.</span>
-              <div className="login-button login-facebook">
-                <i className="fa fa-facebook"></i>
-                <span>CONTINUE WITH FACEBOOK</span>
-              </div>
-              <div className="login-button login-apple">
-                <i className="fa fa-apple"></i>
-                <span>CONTINUE WITH APPLE</span>
-              </div>
-              <div className="login-button login-google">
-                <i className="fa fa-google"></i>
-                <span>CONTINUE WITH GOOGLE</span>
-              </div>
-              <div className="d-flex flex-row">
-                <hr />
-                OR
-                <hr />
-              </div>
-              <div className="input-group">
-                <span>Email address or Username</span>
-                <input placeholder="Email address or username" />
-                <span>Password</span>
-                <input placeholder="Password" type="password" />
-              </div>
-              <p>
-                <a className="forgot-password" href="#">Forget your password?</a>
-              </p>
-              <div className="login-btn">
-                <div className="custom-control custom-checkbox">
-                  <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id="customCheck1"
-                  />
-                  <label className="custom-control-label" for="customCheck1"
-                    >Remember Me</label>
-                </div>
-      
-                <Link to="/home"><div href="#" className="login-button login-spotify">LOG IN</div></Link>
-              </div>
+            <div className="login-button login-apple">
+              <i className="fa fa-apple"></i>
+              <span>CONTINUE WITH APPLE</span>
+            </div>
+            <div className="login-button login-google">
+              <i className="fa fa-google"></i>
+              <span>CONTINUE WITH GOOGLE</span>
+            </div>
+            <div className="d-flex flex-row">
               <hr />
-              <div className="login-footer">
-                <span>Don't have an account?</span>
-                <div className="login-button login-signup">
-                  <span>SIGN UP ON SPOTIFY</span>
-                </div>
+              OR
+              <hr />
+            </div>
+            <Form>
+              <Form.Group>
+                <Form.Label className="mt-3">
+                  Email address or Username
+                </Form.Label>
+                <Form.Control
+                  type="username"
+                  placeholder="Email address or username"
+                  id="username"
+                  value
+                />
+                <Form.Label className="mt-3">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  value
+                />
+                <Form.Text className="text-muted">
+                  Don't share your log-in details with others
+                </Form.Text>
+              </Form.Group>
+               {/* <Form.Group >
+    <Form.Check className="custom-checkbox" type="checkbox" label="Remember Me" id="rememberUser"/>
+  </Form.Group> */}
+              <div className="login-btn">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" for="customCheck1">
+                  Remember Me
+                </label>
+              </div>
+              <Link to="/home">
+                <Button className="login-button login-spotify" type="submit">LOG IN</Button>
+              </Link>
+              </div>
+            </Form>
+
+            
+
+              
+           
+            <hr />
+            <div className="login-footer">
+              <span>Don't have an account?</span>
+              <div className="login-button login-signup">
+                <span>SIGN UP ON SPOTIFY</span>
               </div>
             </div>
-          </section>
-          </body>
-        )
-    }
+          </div>
+        </section>
+      </body>
+    );
+  }
 }
 
-export default Login
+export default withRouter(
+  connect(
+    mapStateToProps
+    // , mapDispatchToProps
+  )(Login)
+);
