c7fed7a049964475a00f38bb4fdc46788d9a68c2
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-02-05 15:53:31 +0000

    api fetch & banner component added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,14 @@ import { Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./CSS/Home.css";
 import Gallery from "./Gallery";
+import UserBanner from "./UserBanner";
+
+//ICONS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserCircle,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 //ACTIONS IMPORTS
 
@@ -63,6 +71,8 @@ class Home extends Component {
 
   render() {
     return (
+      <><UserBanner/>
+    
       <section className="home-body">
         <div className="main-content d-flex flex-column">
           <div className="justify-content-center">
@@ -204,6 +214,7 @@ class Home extends Component {
             )}
         </div>
       </section>
+      </>
     );
   }
 }
